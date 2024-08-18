e9f10c26decdc450f3ca4ad0c48dd7d09b3ad006
zakkycrypt01 zakky <zakkycrypt@gmail.com>
2024-08-18 04:19:45 +0000

    feat: Add createOrder function to backend
    This commit adds a new function called createOrder to the backend code. The function takes an OrderPayload object as input and creates a new order with a unique ID generated using the uuidv4 library. The function performs some validation on the payload and returns a Result object containing the created order and its status. This change is necessary to enable the creation of orders in the application.

--- src/deckahack_backend/src/index.ts
+++ src/deckahack_backend/src/index.ts
@@ -1,7 +1,65 @@
-import { Canister, query, text } from 'azle';
+import { Canister, query, text,update, Record,StableBTreeMap, Null,nat64,Some,Vec, Variant,ic, Principal, Err,Opt, Result, } from 'azle';
+import { uuid as v4 } from 'uuidv4';
+
+
+//order struct
+const Order = Record({
+    id: text,
+    buyer: Principal,
+    status: text,
+    seller: Principal,
+    dispute: text,
+    arbitrator: Principal,
+});
+
+// staus enum
+const Status = Variant({
+    Initated: text,
+    Acknowledged: text,
+    Awaiting_payment: text,
+    Awaiting_release: text,
+    Completed: text,
+    Disputed: text,
+    cancelled: text,
+});
+
+// order payload
+const OrderPayload = Record({
+    buyer: Principal,
+    seller: Principal,
+    status: text,
+    dispute: text,
+    arbitrator: Principal,
+});
+
+// storage
+const storage = StableBTreeMap(0, text, Order);
+
+// time out
+const TIMEOUT_PERIOD = 300000000000n;
+
+
+
+
 
 export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
+    // create order
+    createOrder: update([OrderPayload], Result(Order,Status),(payload) => {
+        if (
+            !payload.buyer ||
+            !payload.seller ||
+            !payload.status ||
+            !payload.dispute ||
+            !payload.arbitrator
+        ){
+            return Err ({InvalidPayload: "Missing required fields"});
+        }
+        const id = v4();
+        const order = {
+            ...payload,
+            orderId : id,
+            
+        }
+    }),
+    
 })
